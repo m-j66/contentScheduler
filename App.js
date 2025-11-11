b25bc81c682375224aad3e08ac0f177fc47cfb74
m-j66 <mahnoor.junaid.19@gmail.com>
2025-11-11 15:55:54 +0500

    Fixed url

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ mongoose
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://my-content-scheduler.vercel.app",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
